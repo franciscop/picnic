929841f259f25d32674871af8c42cfdc4da830aa
FranciscoP <franciscopresencia@hotmail.com>
2015-03-10 15:43:44 +0700

    Added tabs plugin

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,10 @@ var gulp = require('gulp'),
 gulp.task('default', ['build'], function() {});
 
 
-
 // MAIN BUILD
-// First execute the tests
-// Then create the latest versions
-gulp.task('build', ['normal', 'flat', 'plugins'], function() {
-	return gulp.src('./src/picnic.scss')
+// Create the latest versions
+gulp.task('build', function() {
+	return gulp.src('./src/picnic_3.scss')
 		.pipe(sass())
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
@@ -26,44 +24,11 @@ gulp.task('build', ['normal', 'flat', 'plugins'], function() {
 		.pipe(rename('latest.min.css'))
 		.pipe(gulp.dest('./releases'))
 		.pipe(gulp.dest('./web/releases'));
-});
+	});
 
 
 
 // WATCH
 gulp.task('watch', ['build'], function() {
-	gulp.watch(['./src/*.scss'], ['build']);
-});
-
-
-// Build normal one
-gulp.task('normal', [], function() {
-	return gulp.src('./src/picnic.scss')
-		.pipe(sass())
-		.pipe(rename('latest.css'))
-		.pipe(gulp.dest('./web/test/'));;
-});
-
-
-// Build flat mode
-gulp.task('flat', [], function() {
-	return gulp.src('./src/flat.scss')
-		.pipe(sass())
-		.pipe(rename('flat.css'))
-		.pipe(gulp.dest('./web/test/'));;
-});
-
-
-// Build flat mode
-gulp.task('plugins', [], function() {
-	return gulp.src('./src/plugins.scss')
-		.pipe(sass())
-		.pipe(rename('plugins.css'))
-		.pipe(gulp.dest('./releases'))
-		.pipe(gulp.dest('./web/releases'))
-		.pipe(gulp.dest('./web/test/'))
-		.pipe(minify())
-		.pipe(rename('plugins.min.css'))
-		.pipe(gulp.dest('./releases'))
-		.pipe(gulp.dest('./web/releases'));
-});
+	gulp.watch(['./plugins/*/*.scss', './src/*.scss'], ['build']);
+	});
